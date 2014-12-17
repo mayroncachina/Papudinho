4a621e48f4744723ae37633a4266cfff116b1789
Raí Vitor <raivitor@gmail.com>
2014-12-17 14:58:22 -0400

    Atualização
    - Add tela inicial
    - Add navegação entre as telas
    - bug fix google maps

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $( ".home" ).click(function() {
-  alert("back");
+  //alert("back");
   //window.location.replace("#page1");
 });
+
+
+function disabled2 (valor) {
+  if(valor == "Sim"){
+    document.getElementById("vencimento").disabled = false;
+  }
+  else{
+    document.getElementById("vencimento").disabled = true;
+  }
+}
+
+function inicial(){
+	window.location.replace("#page1");
+}
