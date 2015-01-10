4e9f7eb82069a91973ed9baf34a40971407a77c3
Raí Vitor <raivitor@gmail.com>
2015-01-10 19:39:53 -0300

    Correção de interface, novos icones e 2 novas telas
    - Correção de texto que ficava maior que a tela
    - Novos ícones
    - 2 telas: Cadastro de Bar e Clube

--- www/js/app.js
+++ www/js/app.js
@@ -22,19 +22,14 @@ app.run(function($ionicPlatform) {
 });
 
 app.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
     
-    .state('page4', {
+    .state('login', {
       url: '/login',
       templateUrl: 'login'
     })
     
-    .state('page5', {
+    .state('signup', {
       url: '/signup',
       templateUrl: 'signup'
     })
@@ -88,7 +83,80 @@ app.config(function($stateProvider, $urlRouterProvider) {
           templateUrl: "bares"
         }
       }
-    });
+    })
+
+    .state('addClube', {
+      url: '/addClube',
+      templateUrl: 'addClube'
+    })
+
+    .state('addBar', {
+      url: '/addBar',
+      templateUrl: 'addBar'
+    })
+
+    ;
   
   $urlRouterProvider.otherwise('/login');
 });
+
+
+angular.module('ionic.example', ['ionic'])
+
+    .controller('MapCtrl', function($scope, $ionicLoading, $compile) {
+      function initialize() {
+        var myLatlng = new google.maps.LatLng(43.07493,-89.381388);
+        
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+        
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
+
+        var infowindow = new google.maps.InfoWindow({
+          content: compiled[0]
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Uluru (Ayers Rock)'
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.open(map,marker);
+        });
+
+        $scope.map = map;
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+      $scope.centerOnMe = function() {
+        if(!$scope.map) {
+          return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+          content: 'Getting current location...',
+          showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error) {
+          alert('Unable to get location: ' + error.message);
+        });
+      };
+      
+      $scope.clickTest = function() {
+        alert('Example of infowindow with ng-click')
+      };
+      
+    });
